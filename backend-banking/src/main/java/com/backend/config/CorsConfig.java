787e27e6f8d92da5097042cf23d181cc99d06fb4
AshishRajput18 <ashishrajput4999>
2026-01-22 12:11:05 +0530

    Update CORS: allow Vercel subdomains and localhost

--- backend-banking/src/main/java/com/backend/config/CorsConfig.java
+++ backend-banking/src/main/java/com/backend/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+        "https://*.vercel.app",
+        "http://localhost:5173"
+    ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
